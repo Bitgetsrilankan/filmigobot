85f2c0f9b6e9e254f66b47997ece652cf553a829
Neoflix <roxr5650@gmail.com>
2025-02-28 22:55:11 +0530

    Update config.go

--- plugins/config.go
+++ plugins/config.go
@@ -10,7 +10,7 @@ var allTexts map[string]string = map[string]string{
 	"ABOUT": `
 ○ <b>Language</b>: <a href='https://go.dev'>GO</a>
 ○ <b>Library</b>: <a href='https://github.com/PaulSonOfLars/gotgbot'>GoTgbot</a>
-○ <b>Support</b>: <a href='https://FractalProjects'>Fractal</a>
+○ <b>Support</b>: <a href='https://t.me/Cader_r'>BJS Coder</a>
 	`,
 
 	"HELP": `
@@ -35,7 +35,7 @@ Check /help to see how to use me.</i>
 }
 
 var allButtons map[string][][]gotgbot.InlineKeyboardButton = map[string][][]gotgbot.InlineKeyboardButton{
-	"ABOUT": {{homeButton, helpButton}, {{Text: "Source 🔗", Url: "https://github.com/Jisin0/filmigobot"}}},
+	"ABOUT": {{homeButton, helpButton}, {{Text: "🔗 Owner", Url: "https://t.me/Cader_r"}}},
 	"HELP":  append(inlineSearchButtons, []gotgbot.InlineKeyboardButton{aboutButton, homeButton}),
 }
 
